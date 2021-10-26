3146a09b4433d7a71210dced93a1c1fd175faa83
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-26 02:21:51 -0700

    currency conversion feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,10 @@
-var currencyAmountEl = document.getElementById("amount")
-var currencyChoicesEl = document.getElementById("currency-choices")
-var currencyResultEl = document.getElementById("result")
-var currencies = document.querySelectorAll(".currency-choices")
+currencyAmountEl = document.getElementById("amount")
+currencyResultEl = document.getElementById("result")
+currencies = document.querySelectorAll(".currency-choices")
+exchangeButtonEl = document.getElementById("currency-exchange")
+fromCurrencyEl = document.querySelector(".from-currency select")
+toCurrencyEl = document.querySelector(".to-currency select")
 
-var currencyURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
-    fetch(currencyURL).then(function(response){
-     response.json().then(function(results){
-        //  console.log(results);
-         
-     })});
 
 for (let i = 0; i < currencies.length; i++) {
     for (differentCurrencies in PossibleCurrencies) {
@@ -18,3 +14,22 @@ for (let i = 0; i < currencies.length; i++) {
     }
 }     
 
+function convertCurrency(){
+    var currencyAmountVal = currencyAmountEl.value;
+
+
+    var currencyURL = `https://v6.exchangerate-api.com/v6/aa73d9d831b172889cb6b7fb/latest/${fromCurrencyEl.value}`;
+        fetch(currencyURL).then(function(response){
+            response.json().then(function(results){
+        //  console.log(results);
+            var singleRate = results.conversion_rates[toCurrencyEl.value];
+            var totalPrice = (currencyAmountVal * singleRate).toFixed(2);
+            currencyResultEl.innerText = `${currencyAmountVal} ${fromCurrencyEl.value} = ${totalPrice} ${toCurrencyEl.value}`;
+         
+    })});
+}
+
+exchangeButtonEl.addEventListener("click", function(event){
+    event.preventDefault();
+    convertCurrency();
+});
