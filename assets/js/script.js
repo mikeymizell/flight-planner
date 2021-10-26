7d8d31f13483122931c0045f762e7dcaa45a89fd
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-26 16:57:03 -0700

    linked user input to fetch url

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,22 +38,37 @@ exchangeButtonEl.addEventListener("click", function(event){
 
 
 
-var destination = "OAK" + "-sky"
+// var destination = "OAK" + "-sky"
+fromFlightEl = document.getElementById("from-flight");
+toFlightEl = document.getElementById("to-flight");
+departueDateEl = document.getElementById("from-date");
+returnDateEl = document.getElementById("return-date");
+flightQuotesBtn = document.getElementById("flight-quotes");
+resultingFlightEl = document.getElementById("resuling-flight");
 
 
+function flightquotes(){
+    var fromInput = fromFlightEl.value + "-sky";
+    var toInput = toFlightEl.value + "-sky";
+    var departureDateInput = departueDateEl.value;
+    var returnDateInput = returnDateEl.value;
 
-fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + destination +"/JFK-sky/2021-11-01?inboundpartialdate=2021-11-05", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-		"x-rapidapi-key": "13296de5camsh8863768bdc0c0f8p1ad10bjsnec10eb3ec9a4"
-	}
-})
-.then(function(response){
-    response.json().then(function(results){
-        console.log(results)
+
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/"+ fromInput +"/"+ toInput + "/" + departureDateInput +"?inboundpartialdate=" + returnDateInput ,{
+	    "method": "GET",
+	    "headers": {
+		    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		    "x-rapidapi-key": "13296de5camsh8863768bdc0c0f8p1ad10bjsnec10eb3ec9a4"
+	    }
     })
-})
-.catch(err => {
+    .then(function(response){
+        response.json().then(function(results){
+            console.log(results)
+        })
+    })
+    .catch(err => {
 	console.error(err);
-});
+    });
+}
+
+flightQuotesBtn.addEventListener("click", flightquotes);
