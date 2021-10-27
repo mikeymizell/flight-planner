1eac785f9060354009d29b290549cd01f1d891e4
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-26 20:26:46 -0700

    display prices and airlines

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,12 +39,13 @@ exchangeButtonEl.addEventListener("click", function(event){
 
 
 // var destination = "OAK" + "-sky"
-fromFlightEl = document.getElementById("from-flight");
-toFlightEl = document.getElementById("to-flight");
-departueDateEl = document.getElementById("from-date");
-returnDateEl = document.getElementById("return-date");
-flightQuotesBtn = document.getElementById("flight-quotes");
-resultingFlightEl = document.getElementById("resuling-flight");
+var fromFlightEl = document.getElementById("from-flight");
+var toFlightEl = document.getElementById("to-flight");
+var departueDateEl = document.getElementById("from-date");
+var returnDateEl = document.getElementById("return-date");
+var flightQuotesBtn = document.getElementById("flight-quotes");
+var resultingFlightEl = document.getElementById("resuling-flight");
+var currentCountryCurrencyEl= document.getElementById("countrycurrency");
 
 
 function flightquotes(){
@@ -55,17 +56,25 @@ function flightquotes(){
 
 
     fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/"+ fromInput +"/"+ toInput + "/" + departureDateInput +"?inboundpartialdate=" + returnDateInput ,{
-	    "method": "GET",
-	    "headers": {
+	    method: "GET",
+	    headers: {
 		    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
 		    "x-rapidapi-key": "13296de5camsh8863768bdc0c0f8p1ad10bjsnec10eb3ec9a4"
 	    }
     })
     .then(function(response){
-        response.json().then(function(results){
-            console.log(results)
-        })
+       return response.json()
     })
+        .then(function(results){
+            console.log(results);
+            for (let i = 0; i < results.Quotes.length; i++) {
+                var ratesEl = results.Quotes[i].MinPrice;
+                rates = document.createElement("li");
+                rates.textContent = "Your trip will cost "+ ratesEl + "$ through " + results.Carriers[i].Name;
+                resultingFlightEl.appendChild(rates);
+            }
+        })
+    
     .catch(err => {
 	console.error(err);
     });
